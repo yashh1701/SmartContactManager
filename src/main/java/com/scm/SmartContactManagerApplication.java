9f7d56104ce37ea2afd02132b8ac264ae3981b0f
Yash Sherekar <yashsherekar17@gmail.com>
2025-12-28 22:55:35 +0530

    After AWS EBS deployment, working signin google but not githu, manunal signin

--- src/main/java/com/scm/SmartContactManagerApplication.java
+++ src/main/java/com/scm/SmartContactManagerApplication.java
@@ -19,30 +19,32 @@ public class SmartContactManagerApplication implements CommandLineRunner{
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContactManagerApplication.class, args);}
 
-		@Autowired
-		private UserRepo userRepo;
+	@Autowired
+	private UserRepo userRepo;
 
-		@Autowired
-		private PasswordEncoder passwordEncoder;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-		@Override
-		public void run(String... args) throws Exception {
-			User user = new User();
-			user.setUserId(UUID.randomUUID().toString());
-			user.setName("admin");
-			user.setEmail("admin@gmail.com");
-			user.setPassword(passwordEncoder.encode("admin"));
-			user.setRoleList(List.of(AppConstants.ROLE_USER));
-			user.setEmailVerified(true);
-			user.setEnabled(true);
-			user.setAbout("This is dummy user created initially");
-			user.setPhoneVerified(true);
+	@Override
+	public void run(String... args) throws Exception {
+		User user = new User();
+		user.setUserId(UUID.randomUUID().toString());
+		user.setName("admin");
+		user.setEmail("admin@gmail.com");
+		user.setPassword(passwordEncoder.encode("admin"));
+		user.setRoleList(List.of(AppConstants.ROLE_USER));
+		user.setEmailVerified(true);
+		user.setEnabled(true);
+		user.setAbout("This is dummy user created initially");
+		user.setPhoneVerified(true);
 
-			userRepo.findByEmail("admin@gmail.com").ifPresentOrElse(user1 -> {},() -> {
-				userRepo.save(user);
-				System.out.println("user created");
-			});
-		
+		userRepo.findByEmail("admin@gmail.com").ifPresentOrElse(user1 -> {},() -> {
+			userRepo.save(user);
+			System.out.println("user created");
+		});
+
+
+
 	}
 
 }
